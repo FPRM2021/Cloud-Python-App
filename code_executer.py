39ef9810438596c3c7acb9b4de865d5a1ea3518f
FPRM2021 <37373367+FPRM2021@users.noreply.github.com>
2023-06-17 14:33:18 -0500

    second commit
    Added the flask app to the code executer. Now the script runs in the flask server and uses the html templates

--- code_executer.py
+++ code_executer.py
@@ -1,5 +1,21 @@
-def execute_python_code(code):
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    code = request.form.get('code')
     try:
         exec(code)
+        result = "Code executed successfully."
     except Exception as e:
-        print("An error occurred:", e)
+        result = f"An error occurred: {e}"
+    
+    return render_template('result.html', result=result)
+
+if __name__ == '__main__':
+    app.run()
