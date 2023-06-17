d9e77c5fba84d321b431a31305a864862595b6d0
FPRM2021 <37373367+FPRM2021@users.noreply.github.com>
2023-06-17 14:41:19 -0500

    third commit
    Now the result throws back the output from the code.

--- code_executer.py
+++ code_executer.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import sys
+from io import StringIO
 
 app = Flask(__name__)
 
@@ -9,13 +11,26 @@ def home():
 @app.route('/execute', methods=['POST'])
 def execute():
     code = request.form.get('code')
+    
     try:
+        # Redirect standard output to capture the output
+        stdout = sys.stdout
+        sys.stdout = StringIO()
+
         exec(code)
+        
+        # Get the captured output
+        output = sys.stdout.getvalue()
+        
         result = "Code executed successfully."
     except Exception as e:
+        output = ""
         result = f"An error occurred: {e}"
     
-    return render_template('result.html', result=result)
+    # Reset the standard output
+    sys.stdout = stdout
+    
+    return render_template('result.html', result=result, output=output)
 
 if __name__ == '__main__':
     app.run()
